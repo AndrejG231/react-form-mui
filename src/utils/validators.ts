ea10e61beda568307e2af0e7fd91e3c3288a6b6d
Andrej Germič <69108774+AndrejG231@users.noreply.github.com>
2023-10-11 13:44:02 +0200

    Add regex field validators

---
+++ src/utils/validators.ts
@@ -0,0 +1,5 @@
+export const emailValidationRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+export const phoneValidationRegex = /^(?:[0-9] ?){6,14}[0-9]$/;
+
+export const digitsValidationRegex = /^[0-9]+$/;
