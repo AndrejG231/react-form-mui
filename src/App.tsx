5b280095a8436dabbeab01dae8ad70788630d3d3
Andrej Germič <69108774+AndrejG231@users.noreply.github.com>
2023-10-11 13:44:45 +0200

    Cleanup base directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "@mui/material";
+
+import SomeFormPage from "pages/some-form-page";
+
+import theme from "styles/theme";
+
+import TranslationsProvider from "translations/TranslationsProvider";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <TranslationsProvider>
+                <main>
+                    <SomeFormPage />
+                </main>
+            </TranslationsProvider>
+        </ThemeProvider>
+    );
 }
 
 export default App;
